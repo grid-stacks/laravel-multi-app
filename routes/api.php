c90eff7d53db8974ae671d2e3f9e84ed19f122b8
Sajib Hasan <sajib001@gmail.com>
2021-08-08 02:43:48 +0600

    Claim custom claims from request

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Lcobucci\JWT\Configuration;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:api', 'claim:type,admin'])->get('/user', function (Request $request) {
+    $token = $request->bearerToken();
+    $jwt = Configuration::forUnsecuredSigner()->parser()->parse($token);
+    $type = $jwt->claims()->get('type');
+
+    return [
+        'user' => $request->user(),
+        'type' => $type
+    ];
 });
